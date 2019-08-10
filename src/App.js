603461a60183e8ea7870698bfe2978ec47b9fdcd
viniciusromaodossantos <viniromasan@poli.ufrj.br>
2019-08-10 18:41:41 -0300

    Primeiro Commit

---
+++ src/App.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+   
+        
+        <code
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Eu amo a minha namorada Thais
+        </code>
+      </header>
+    </div>
+  );
+}
+
+export default App;
