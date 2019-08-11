3c9df64b8603530c1ae7d9470b3a2d7911bf354d
viniciusromaodossantos <viniromasan@poli.ufrj.br>
2019-08-10 21:54:07 -0300

    Updates

---
+++ precache-manifest.e199b0dc3abf97718d7bf96816d58e7d.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b15a4f986fd5639262f234b19e5ddc2e",
+    "url": "/viniciusromaodossantos/CV.git/index.html"
+  },
+  {
+    "revision": "7564421b0810c5ad41f6",
+    "url": "/viniciusromaodossantos/CV.git/static/css/main.5c67aeb6.chunk.css"
+  },
+  {
+    "revision": "51ee03673b99ae34a20c",
+    "url": "/viniciusromaodossantos/CV.git/static/js/2.ad7aca24.chunk.js"
+  },
+  {
+    "revision": "7564421b0810c5ad41f6",
+    "url": "/viniciusromaodossantos/CV.git/static/js/main.3fa657f6.chunk.js"
+  },
+  {
+    "revision": "84e772673636934e2e15",
+    "url": "/viniciusromaodossantos/CV.git/static/js/runtime~main.51e94226.js"
+  }
+]);
